7ae377cdc8c490feb408f14e1a5bb932215e1e44
Zombox <ghostzb@outlook.com>
2023-12-04 22:16:27 +0700

    Initial Commit

---
+++ src/main/c/b/a/FirstClass.java
@@ -0,0 +1,14 @@
+package c.b.a;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("shift +  f6 ทำการ rename");
+        System.out.println("ctrl + alt + O เอา import ที่ไม่ import ที่ไม่ได้ใช้ออก");
+        System.out.println("shift + shift ค้นหาไฟล์");
+        System.out.println("ctrl + shift + f ค้นหา text");
+        System.out.println("ctrl + alt + l จัดเรียง");
+        System.out.println("ctrl + d copy ลงมา");
+        System.out.println("alt + shift + ลูกศร ขึ้นลงมาบรรทัด");
+        System.out.println("ctrl + g ไปบรรทัดที่ต้องการ");
+    }
+}
